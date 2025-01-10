2d3126877516881520c702cc4e41f591827fef23
jaykit <jaykitmaurya19@gmail.com>
2025-01-10 19:50:54 +0530

    frontend added

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const authenticate=require("./Mongo/Authentication.js");
 // const gTTS = require("gtts");
 
 const corsOptions = {
-  //origin:"https://multilanguage-translator-mern-client.vercel.app",
-  origin:"http://localhost:3000",
+  origin:"https://multilanguage-translator-mern-client.vercel.app",
+//  origin:"http://localhost:3000",
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true,
 };
