2ad5c19c5548c7294d25ff3f58c41d5342476692
jaykit <jaykitmaurya19@gmail.com>
2025-01-12 19:35:37 +0530

    only url updated

--- Mongo/HistoryModel.js
+++ Mongo/HistoryModel.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://jaykit1907:NLeC4KaUBDCFaPYh@cluster0.h1mdt.mongodb.net/jaykit1?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>{console.log("connected history successfulllly.....")}).catch((err)=>{console.log("error",err)});
+
+mongoose.connect("mongodb://localhost:27017/Translator1").then(()=>{
+    console.log("connected history successfully...");
+}).catch((err)=>{
+    console.log("error at mongodb",err);
+})
+
+
+
+
+
+// mongoose.connect("mongodb+srv://jaykit1907:NLeC4KaUBDCFaPYh@cluster0.h1mdt.mongodb.net/jaykit1?retryWrites=true&w=majority&appName=Cluster0")
+// .then(()=>{console.log("connected history successfulllly.....")}).catch((err)=>{console.log("error",err)});
 
 const historySchema = new mongoose.Schema({
     email: { type: String, required: true },
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const authenticate=require("./Mongo/Authentication.js");
 // const gTTS = require("gtts");
 
 const corsOptions = {
-  origin:"https://multilanguage-translator-mern-client.vercel.app",
+ origin:"https://multilanguage-translator-mern-client.vercel.app",
  //origin:"http://localhost:3000",
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true,
